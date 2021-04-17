ba3fe202785d4da25d87ffdbab8936d9ac7fece0
Ciro Viana <cirotviana@gmail.com>
2021-04-17 18:52:36 -0300

    React front-end installed.

---
+++ front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PrimarySearchAppBar from './components/CompaniesAppBar';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <PrimarySearchAppBar></PrimarySearchAppBar>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
